e7d66082aad9956a69bfeb2a07ba2ad9627b76c0
Rafael <rafael@hurpia.com.br>
2016-07-24 14:00:35 -0300

    Set requirements for last meteor package

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.2.1');
+  api.versionsFrom('1.3.5.1');
   api.addFiles('rafaelhdr:google-charts.js');
   api.export(['drawChart'], 'client');
 });
