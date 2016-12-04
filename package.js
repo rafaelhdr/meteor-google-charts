33308e17c73546fbc06a34727b7fdaadf3812a9f
Rafael <rafael@hurpia.com.br>
2016-12-04 17:52:17 +0000

    Stop using document.write

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'rafaelhdr:google-charts',
   summary: 'Easy Google Charts for Meteor',
-  version: '0.0.6',
+  version: '0.0.7',
   git: 'https://github.com/rafaelhdr/meteor-google-charts.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3.5.1');
+  api.versionsFrom('1.4.2');
   api.addFiles('rafaelhdr_google-charts.js');
   api.export(['drawChart'], 'client');
 });
